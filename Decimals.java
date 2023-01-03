b0240199c453798c4c27138d2c22fc0104ff6a7a
John Stewart <jstewartstack@gmail.com>
2023-01-03 21:11:39 +0000

    finished

---
+++ Decimals.java
@@ -0,0 +1,13 @@
+public class Decimals {
+
+     public static void main(String[] args) {
+          
+          double percentage = 73.5;
+          System.out.println(percentage + " of percentages are made up");
+
+          double wallet = 20; // Always use doubles for math calcs. This will be stored as double
+          int people = 3;
+          System.out.println(wallet / people);
+     }
+     
+}
