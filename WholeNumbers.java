a77a2a6066ac26f672ebb10076aa1a53a4467297
John Stewart <jstewartstack@gmail.com>
2023-01-03 20:59:47 +0000

    how long works

---
+++ WholeNumbers.java
@@ -0,0 +1,10 @@
+public class WholeNumbers {
+    
+     public static void main(String[] args) {
+          long globalPopulation = 8_000_000_000L;//underscores added for readability
+          System.out.println("The global populations is " + globalPopulation);
+          
+          long dailyGoogleSearches = 8_500_000_0000L;
+          System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+     }
+}
