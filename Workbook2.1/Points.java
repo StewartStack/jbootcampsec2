ea683b9c35b82ce1a294bc4fd650775db19a0fc4
John Stewart <jstewartstack@gmail.com>
2023-01-03 14:41:10 +0000

    fixed again

--- Workbook2.1/Points.java
+++ Workbook2.1/Points.java
@@ -5,17 +5,16 @@ public class Points {
 
         int points = 0;
 
-        points -= 50;
         System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
-        points -= 3;
+        //update points here
         System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
-        points += 30;
+        //update points here
         System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
-        points += 100;
+        //update points here
         System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
-        points += 60;    
+        //update points here
         System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
-        
+        //update points here        
         
         // Compare your result to what's on Learn the Part.
         
