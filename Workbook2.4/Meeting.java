4292d4e8aca372b9f2018bc897d3dd22730a0787
John Stewart <jstewartstack@gmail.com>
2023-01-04 17:21:37 +0000

    workbook finished

---
+++ Workbook2.4/Meeting.java
@@ -0,0 +1,22 @@
+public class Meeting {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.4 to access the link).
+
+        double sales = 24309.65;
+        int totalSales = (int)sales;
+        double profit = 18562.18;
+        int totalProfit = (int)profit; 
+        double refunds = 688.78;
+        int refundTotal = (int)refunds;
+        double shipping = 1233.57;
+        int totalShipping = (int)shipping; 
+
+        System.out.println("This month, we made $" + totalSales + " in sales");
+        System.out.println("Factoring in costs, we made $" + totalProfit + " in profit");
+        System.out.println("The refunds are at a low $" + refundTotal + ". This is a good sign!");
+        System.out.println("Shipping costs were high. We paid $" + totalShipping + " in shipping");
+
+
+    }
+}
