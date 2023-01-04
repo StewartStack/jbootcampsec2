bc4c89d90dadd932088c56a384321bd919d8cbd1
John Stewart <jstewartstack@gmail.com>
2023-01-04 16:33:51 +0000

    added some comments

--- Workbook2.3/AppleStore.java
+++ Workbook2.3/AppleStore.java
@@ -15,17 +15,17 @@ public class AppleStore {
         System.out.println("One customer walked in. He bought 4 apples!");
         profit += 4 * priceOfApple;
         numOfApples -= 4;
-        numOfCustomers += 1;
+        numOfCustomers++;//will add 1 customer.
 
         System.out.println("Another customer walked in. He bought 20 apples!");
         profit += 20 * priceOfApple ;
         numOfApples -= 20;
-        numOfCustomers += 1;
+        numOfCustomers++;//will add 1 customer.
 
         System.out.println("Another customer walked in. She bought 200 apples!");
         profit += 200 * priceOfApple;
         numOfApples -= 200;
-        numOfCustomers += 1;
+        numOfCustomers++;//will add 1 customer
         
         System.out.println("Wow! So far, you made: $" + profit);
         System.out.println(numOfCustomers + " customers love your apples.");
