0e7920991f7b37ed0a22078a1957a3bf10352d2d
John Stewart <jstewartstack@gmail.com>
2023-01-04 19:50:55 +0000

    finished

--- Survey.java
+++ Survey.java
@@ -9,32 +9,32 @@ public class Survey {
         Scanner scanner = new Scanner(System.in);
         int counter = 0;
 
-        System.out.println("What is your name?");
+        System.out.println("\nWhat is your name?");
         String name = scanner.nextLine();
+        counter++;
 
-        System.out.println("How much money do you spend on coffee?");
+        System.out.println("\nHow much money do you spend on coffee?");
         double coffeePrice = scanner.nextDouble();
         counter++;
 
-        System.out.println("How much money do you spend on fast food?");
+        System.out.println("\nHow much money do you spend on fast food?");
         double foodPrice = scanner.nextDouble();
         counter++;
 
-        System.out.println("How many times a week do you buy coffee?");
+        System.out.println("\nHow many times a week do you buy coffee?");
         int coffeeAmount = scanner.nextInt();
         counter++;
 
-        System.out.println("How many times a week do you buy fast food?");
+        System.out.println("\nHow many times a week do you buy fast food?");
         int foodAmount = scanner.nextInt();
         counter++;
 
         scanner.close();//prevent resourse leak?
 
         //*********PART B: RESPONDING TO THE USER**********
-        System.out.println("Thank you " + name + " for answering all " + counter + " questions");//counter will answer how many Qs were answered
-
-        System.out.println("Weekly, you spend $" + coffeeAmount + " on coffee");
-        System.out.println("Weekly, you spend $" + foodAmount + " on food");
+        System.out.println("\nThank you " + name + " for answering all " + counter + " questions");//counter will answer how many Qs were answered
+        System.out.println("Weekly, you spend $" + (coffeeAmount * coffeePrice) + " on coffee");
+        System.out.println("Weekly, you spend $" + (foodAmount * foodPrice) + " on food");
 
     }
 }
