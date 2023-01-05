a194a28befac769e9172b9c42cc2324153278847
John Stewart <jstewartstack@gmail.com>
2023-01-05 16:16:26 +0000

    finished

--- Challenge2/Chatbot.java
+++ Challenge2/Chatbot.java
@@ -30,12 +30,14 @@ public class Chatbot {
         System.out.println("This means I'm "+ (400 / age) + " times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
-
+        scan.nextLine();
+        String language = scan.nextLine();
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.println("\n" + language + ", that's great! Nice chatting with you " + name + ". I have to log off now. See ya!");
         
         //close scanner. 
+        scan.close();
 
         
     }
