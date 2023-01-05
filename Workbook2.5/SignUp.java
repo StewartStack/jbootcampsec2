2360094a74313df5c98d938a4a6f99a24a233d8c
John Stewart <jstewartstack@gmail.com>
2023-01-05 15:33:38 +0000

    fixed with nextline trap sorted

--- Workbook2.5/SignUp.java
+++ Workbook2.5/SignUp.java
@@ -21,6 +21,7 @@ public class SignUp {
 
         System.out.println("Make a username");  
         // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
+        String throwAway = scan.nextLine();//This is to resolve trap?
         String userName = scan.nextLine();       
 
         System.out.println("What city do you live in?");
