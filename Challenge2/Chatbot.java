dfea321dd429172d156d4e28b93b585051a19998
John Stewart <jstewartstack@gmail.com>
2023-01-05 16:12:44 +0000

    age question added

--- Challenge2/Chatbot.java
+++ Challenge2/Chatbot.java
@@ -22,11 +22,12 @@ public class Chatbot {
         System.out.println("\nI hear it's beautiful at " + home + "! I'm from a place called Oracle");
         System.out.println("How old are you?");
         //Pick up age and store it.
+        int age = scan.nextInt();
 
 
         //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("\nSo you're " + age + ", cool! I'm 400 years old.");
+        System.out.println("This means I'm "+ (400 / age) + " times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
 
