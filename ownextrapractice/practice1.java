cd11cdb47c9246ad42ed2ebe254b1e48778e9eb6
John Stewart <jstewartstack@gmail.com>
2023-01-07 21:32:33 +0000

    starting a simple calculator in java

--- ownextrapractice/practice1.java
+++ ownextrapractice/practice1.java
@@ -1,8 +1,10 @@
 package ownextrapractice;
-
+/*
+ * Simple calculator in java
+ */
 public class practice1 {
     public static void main(String[] args) {
-     
+             
           
     } 
 }
